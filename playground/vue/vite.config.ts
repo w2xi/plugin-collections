28eff2e72f6649df1715f421138532b5c165ef5d
w2xi <43wangxi@gmail.com>
2024-11-05 23:30:32 +0800

    chore: configure vite-plugin-inspect

--- playground/vue/vite.config.ts
+++ playground/vue/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgv from 'vite-plugin-svgv'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [vue(), svgv()],
+  plugins: [vue(), svgv(), Inspect()],
 })
