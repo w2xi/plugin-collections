fd7f51aa6f72339862851ebd3742f316b0d0373f
w2xi <43wangxi@gmail.com>
2024-11-05 19:40:48 +0800

    feat: enhance ts declarations

---
+++ packages/vite-plugin-svgv/client.d.ts
@@ -0,0 +1,15 @@
+declare module '*.svg?vue' {
+  import { FunctionalComponent, SVGAttributes } from 'vue'
+  const src: FunctionalComponent<SVGAttributes>
+  export default src
+}
+
+declare module '*.svg?url' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg?raw' {
+  const src: string
+  export default src
+}
