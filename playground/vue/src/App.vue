d8769e7f498d521f8084e9fa05d7a2f0c6531ae0
w2xi <43wangxi@gmail.com>
2024-11-05 19:41:33 +0800

    chore: vue playground

--- playground/vue/src/App.vue
+++ playground/vue/src/App.vue
@@ -1,3 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import vueLogo from './assets/logo.svg'
+import vueLogoComponent from './assets/logo.svg?vue'
+import vueLogoRaw from './assets/logo.svg?raw'
+import vueLogoUrl from './assets/logo.svg?url'
+</script>
 
-<template>App</template>
+<template>
+  <div class="default">
+    <img :src="vueLogo" />
+  </div>
+  <div class="component">
+    <vueLogoComponent />
+  </div>
+  <div class="raw">
+    <pre>{{ vueLogoRaw }}</pre>
+  </div>
+  <div class="url">
+    {{ vueLogoUrl }}
+  </div>
+</template>
