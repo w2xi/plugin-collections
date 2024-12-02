43a1e8f71b57512dcf70ead358514b016a1568b7
w2xi <43wangxi@gmail.com>
2024-12-02 20:55:12 +0800

    chore: adjust deps

--- packages/plugin-px2rem/magic-string.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'magic-string' {
-  export default class MagicString {
-    constructor(str: string)
-    update(start: number, end: number, content: string): void
-    toString(): string
-  }
-}
